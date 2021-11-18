ac3190baeb11f56010b8940b29f42e5577745a70
Aristolon <drstar515@gmail.com>
2021-11-18 18:46:45 +0800

    editwallpapersurl

--- wp-change.sh
+++ wp-change.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 feh --recursive --randomize --bg-fill ~/Pictures/wallpapers
-#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/ghibili
-#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
